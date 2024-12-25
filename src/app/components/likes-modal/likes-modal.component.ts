53544dfa6d686fc8d18d71059d523e20485363c3
fyambos <feliciteyambos@gmail.com>
2024-12-25 23:17:24 +0100

    ui: fix likes modal and redirect to profile on click

--- src/app/components/likes-modal/likes-modal.component.ts
+++ src/app/components/likes-modal/likes-modal.component.ts
@@ -1,11 +1,19 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { User } from 'src/app/models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-likes-modal',
   templateUrl: './likes-modal.component.html',
 })
 export class LikesModalComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { users: User[] }) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { users: User[] },
+    private router: Router,
+  ) {}
+
+  navigateToUserProfile(userId: string): void {
+    this.router.navigate([`/profile/${userId}`]);
+  }
 }
